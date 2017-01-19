33b1b1e8bf6343cd875d197229846c939329ab42
michaelpacer <michaelpacer@gmail.com>
2017-01-19 15:58:43 -0800

    MNT: Re-rendered with conda-smithy 2.0.0

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -53,5 +53,4 @@ source run_conda_forge_build_setup
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
 EOF
